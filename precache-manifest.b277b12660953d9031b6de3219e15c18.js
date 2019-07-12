f41ada42d748296c2085b8694f65aad91819f767
nikitaomg <50542976+nikitaomg@users.noreply.github.com>
2019-07-12 15:39:27 +0300

    Add files via upload

---
+++ precache-manifest.b277b12660953d9031b6de3219e15c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd00179d74760f90f141",
+    "url": "/static/js/main.206d112d.chunk.js"
+  },
+  {
+    "revision": "af3c7d39417cfe7b4acf",
+    "url": "/static/js/2.6f669d62.chunk.js"
+  },
+  {
+    "revision": "bd00179d74760f90f141",
+    "url": "/static/css/main.ca27c372.chunk.css"
+  },
+  {
+    "revision": "af3c7d39417cfe7b4acf",
+    "url": "/static/css/2.e0b689d8.chunk.css"
+  },
+  {
+    "revision": "6e648c4371d07ac87eb81fff0955ca3b",
+    "url": "/index.html"
+  }
+];
