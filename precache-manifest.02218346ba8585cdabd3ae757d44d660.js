2dbbd25e499875b611f01e3721ecf0fcfe974911
nikitaomg <50542976+nikitaomg@users.noreply.github.com>
2019-07-17 09:55:49 +0300

    Add files via upload

---
+++ precache-manifest.02218346ba8585cdabd3ae757d44d660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8aec5e06f383666383e5",
+    "url": "/static/js/main.482ee523.chunk.js"
+  },
+  {
+    "revision": "1be117d9c5980a6e8cb8",
+    "url": "/static/js/2.66e8c82f.chunk.js"
+  },
+  {
+    "revision": "8aec5e06f383666383e5",
+    "url": "/static/css/main.158e008d.chunk.css"
+  },
+  {
+    "revision": "1be117d9c5980a6e8cb8",
+    "url": "/static/css/2.e0b689d8.chunk.css"
+  },
+  {
+    "revision": "6ae039e5c3dcff4e38b82e319c12be70",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9aeac790641bbebcc395665191d1f50c.js"
+  "/precache-manifest.02218346ba8585cdabd3ae757d44d660.js"
 );
 
 workbox.clientsClaim();
