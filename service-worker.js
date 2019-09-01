adaf785261a2c1d1d496a4a8b2de508c11d091c3
nikitaomg <50542976+nikitaomg@users.noreply.github.com>
2019-09-01 17:46:37 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67c7bb0e87f59643b65c7804e6454d56.js"
+  "/precache-manifest.23dc4eb8deced2d24e3abc7133a5b092.js"
 );
 
 workbox.clientsClaim();
